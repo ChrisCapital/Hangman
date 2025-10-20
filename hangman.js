3dc9b36e0df161608e56aa470fa282c382d68752
ChrisCapital <chris.shavuo@gmail.com>
2025-10-20 10:06:50 -0400

    added newgame function and button in html

--- hangman.js
+++ hangman.js
@@ -1,4 +1,18 @@
 var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenstrate", "obsequious", "dissonant", "toady", "iempotent"];
 
-var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+
+var word = "";
+
+
+function newGame(){
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS[randomIndex];
+
+    var clueString = "_";
+    for (var i = 1; i < word.length; i++){
+        clueString += " _";
+    }
+    var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+
+}
