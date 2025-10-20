391f17c498ba5e8b6f226bd6548fe1b149bb466e
ChrisCapital <chris.shavuo@gmail.com>
2025-10-20 10:34:04 -0400

    added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -2,17 +2,40 @@ var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenstrate", "obsequious", 
 
 
 var word = "";
-
+var guesses = "";
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    updatePage();
 
+}
+
+function guessLetter(){
+    var input = document.getElementById("guess");
+    var letter = input.value;    
+    guesses += letter;
+    updatePage();
+}
+
+
+
+
+function updatePage(){
     var clueString = "_";
     for (var i = 1; i < word.length; i++){
-        clueString += " _";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter)>=0){
+            clueString += currentLetter + "";
+        }else{
+            clueString += " _";
+        }
+        
     }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guesses:" + guesses;
 }
