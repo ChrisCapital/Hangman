cce63e2897b94e8af9926fad4c0255b6e9a92b7d
ChrisCapital <chris.shavuo@gmail.com>
2025-10-22 10:25:17 -0400

    fixed some erros

--- hangman.js
+++ hangman.js
@@ -3,12 +3,12 @@ var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenstrate", "obsequious", 
 
 var word = "";
 var guesses = "";
+var guess_count = MAX_GUESSES;
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
-    guess_count = MAX_GUESSES;
     updatePage();
 
 }
@@ -17,6 +17,7 @@ function guessLetter(){
     var input = document.getElementById("guess");
     var letter = input.value;
     if(word.indexOf(letter)<0){
+        console.log("Wrong guess!");
         guess_count--;
     }   
     guesses += letter;
@@ -26,6 +27,7 @@ function guessLetter(){
 
 
 function updatePage(){
+
     var clueString = "_";
     for (var i = 1; i < word.length; i++){
         var currentLetter = word.charAt(i);
@@ -41,7 +43,7 @@ function updatePage(){
     clue.innerHTML = clueString;
 
     var guessArea = document.getElementById("guesses");
-    guessArea.innerHTML = "Guesses:" + guesses;
+    guessArea.innerHTML = "Guessed Letters:" + guesses;
 
     var image = document.getElementById("hangmanImage");
     image.src = "images/hangman" + guess_count + ".gif";
