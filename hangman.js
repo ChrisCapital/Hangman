a4a7018ce3e9563b235c95762ce5716c98428bf8
ChrisCapital <chris.shavuo@gmail.com>
2025-11-05 16:30:42 -0500

    added game over

--- hangman.js
+++ hangman.js
@@ -1,21 +1,24 @@
 var POSSIBLE_WORDS = ["obdurate", "versimilitude", "defenstrate", "obsequious", "dissonant", "toady", "iempotent"];
 
 
+var MAX_GUESSES = 6;
 var word = "";
 var guesses = "";
 var guess_count = MAX_GUESSES;
+gameWon = false;
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
+    guess_count = MAX_GUESSES;
     updatePage();
 
 }
 
 function guessLetter(){
     var input = document.getElementById("guess");
-    var letter = input.value;
+    var letter = input.value.toLowerCase();
     if(word.indexOf(letter)<0){
         console.log("Wrong guess!");
         guess_count--;
@@ -23,19 +26,31 @@ function guessLetter(){
     guesses += letter;
     updatePage();
     input.value = "";
+    if (!word){
+        document.getElementById("guessess").textContent = "Start a new game first"; 
+        return;
+    }
+    if (typeof gameOver === 'undefined') {
+        gameOver = false;
+    }
+    if (gameover){
+        document.getElementById("guesses").textContent = "The game is over. Please start a new game.";
+        return;
+    }
+
 }
 
 
 function updatePage(){
 
     var clueString = "_";
-    for (var i = 1; i < word.length; i++){
+    for (var i = 0; i < word.length; i++){
         var currentLetter = word.charAt(i);
         if(guesses.indexOf(currentLetter)>=0){
             clueString += currentLetter + "";
         }
         else{
-            clueString += " _";
+            clueString += "_ ";
         }
         
     }
